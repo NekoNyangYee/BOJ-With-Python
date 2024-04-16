8284952b9b9fc5dcb33ff4e1f46bdaeda222015d
NekoNyangYee <78199616+NekoNyangYee@users.noreply.github.com>
2024-04-17 00:17:06 +0900

    2525 upload 2

--- 2525.py
+++ 2525.py
@@ -1,11 +1,16 @@
-time = ["23", "48", "25"]
-h = int(time[0])
-m = int(time[1])
-after = int(time[2])
+# 1. 시간, 분을 먼저 받아오기
+H, M = map(int, input().split())
+# 2. 더할 시간을 받아오기
+timer = int(input()) 
 
-if m + after < 60:
-    print(h, m + after)
-elif m + after >= 120:
-    print(h + 2, m + after - 120)
-elif m + after >= 60 and h == 23:
-    print(24 - (h + 1), m + after - 60)
+# 3. 받아온 타이머에 60을 나눈 몫을 H에, 60을 나눈 나머지를 M에 더하기
+H += timer // 60
+M += timer % 60
+
+# 4. M이 60이상이면 H에 1을 더하고 M에서 60을 빼기, H가 24이상이면 H에서 24를 빼기
+if M >= 60:
+    H += 1
+    M -= 60
+if H >= 24:
+    H -= 24
+print(H,M)

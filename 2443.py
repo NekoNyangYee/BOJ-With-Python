ba804f90b6f4a801ef9ffeece4c8d317e54aa65a
NekoNyangYee <78199616+NekoNyangYee@users.noreply.github.com>
2024-05-13 00:20:05 +0900

    05/12 uploads 2

--- 2443.py
+++ 2443.py
@@ -1,5 +1,5 @@
 n = int(input())
 
 for i in range(1, n + 1):
-    print(" " * (n - i), end="")
-    print("*" * (2 * (n - (n - i)) - 1))
+    print(" " * (i - 1), end="")
+    print("*" * (2 * (n - i) + 1))
